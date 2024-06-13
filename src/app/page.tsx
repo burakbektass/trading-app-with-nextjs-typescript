8800d4d0049adc5df4fb61823bbb533b7270c93c
burakbektass <marshall_maters_1972@hotmail.com>
2024-06-13 23:41:18 +0300

    Project initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>First commit of trading application</h1>
+      </div>
+    </main>
+  );
+}
